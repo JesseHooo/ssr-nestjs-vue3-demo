7babd493e6b30cb1bedb74ca0200b1c49d19db8f
hejiecong <hejc@rhb-tech.com>
2024-06-20 14:17:33 +0800

    update: 复现问题部分

--- config.ts
+++ config.ts
@@ -1,7 +1,17 @@
 import type { UserConfig } from 'ssr-types'
 
 const userConfig: UserConfig = {
-
+    serverPort: 14002,
+    isVite: true,
+    viteConfig: () => {
+        return {
+            client: {
+                otherConfig: {
+                    build: { sourcemap: true }
+                }
+            }
+        }
+    }
 }
 
 export { userConfig }
